6ae91f51abaa0e2903c8a4de3825ec555745be30
vieiramaria <duda.cv2404@gmail.com>
2022-10-25 10:50:43 -0300

    Adicionada linha com versão da aplicação.

--- ControleDeVersaoComGit/Program.cs
+++ ControleDeVersaoComGit/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine(DateTime.Now.ToLongDateString());
+Console.WriteLine("Versão da aplicação: 1.0");
